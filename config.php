7a1c573e267786b9ef706e628fc4e8a14ce3f318
claudiajazmin19 <jazminsoria191998@gmail.com>
2021-01-10 18:55:44 +0100

    commit

--- config.php
+++ config.php
@@ -1,10 +1,12 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "count_on_me");
+$host = "localhost";
+$dbuser = "root";
+$dbpwd = "";
+$db = "count_on_me";
 
-if(mysqli_connect_error()){
-    echo "Failed to connect to MySql: ". mysqli_connect_error();
-    die();
-}
-$error="";
+$connect = new mysqli($host, $dbuser, $dbpwd, $db);
+	if($connect->connect_errno){
+        die("La conexión a fallado");
+    }
 ?>
 
--- login.php
+++ login.php
@@ -1,11 +1,38 @@
+<?php
+    session_start();
+    if(isset($_SESSION['usuario'])){
+        header("Location: index.php");
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
-    hoal
+    <form action="" method="post">
+        <input type="text" placeholder="Usuario" name="usuario" pattern="[A-Za-z_-0-9]{1,20}">
+        <input type="password"  placeholder="Contraseña" name="contrasena" pattern="[A-Za-z_-0-9]{1,20}">
+        <input type="submit" name="login" value="Iniciar Sesión">
+    </form>
+
+    <?php
+        if(isset($_POST['login'])){
+            require ("config.php");
+            $usuario = $_POST['usuario'];
+            $contrasena = $_POST['contrasena'];
+
+            $validar = $connect->query("SELECT  * FROM usuarios WHERE usuario = '$usuario' AND  contrasena ='$contrasena'");
+            $contar = $validar->num_rows;
+
+            if ($contar == 1){
+                $_SESSION['usuario'] = $usuario;
+                header("Location: index.php");
+            }
+        }
+
+    ?>
 </body>
 </html>
