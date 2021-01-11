bb391b161b42994d6a3aef3942098ec234d66c77
Claudia Jazmín Soria Saavedra <jazminsoria191998@gmail.com>
2021-01-11 10:26:43 +0100

    commit

--- editarPerfil.php
+++ editarPerfil.php
@@ -25,42 +25,8 @@
         <textarea rows="10" cols="50" placeholder="Describe para que los demás conozcan un poco sobre ti."></textarea>
         <input type="submit" name="reg" value="Actualizar Perfil">
     </form>
+    <br>
+    <a href='index.php'> Regresar </a>    
 
-    <?php
-        if(isset($_POST['reg'])){
-
-            require("config.php");
-
-            $nombre = $_POST['nombre'];
-            $email = $_POST['email'];
-            $usuario = $_POST['usuario'];
-            $contrasena = md5($_POST['contrasena']);
-            $repcontrasena = md5($_POST['repcontrasena']);
-            
-
-            $consulta = $connect->query("SELECT * FROM usuarios WHERE usuario = '$usuario'");
-            $contarusuario = $consulta->num_rows;
-
-            $consulta = $connect->query("SELECT * FROM usuarios WHERE email = '$email'");
-            $contaremail = $consulta->num_rows;
-
-            if($contarusuario == 0 && $contaremail == 0 && $contrasena == $repcontrasena){
-                $insertar = $connect->query("INSERT INTO usuarios (nombre, usuario, contrasena, email, fecha_reg) VALUES ('$nombre','$usuario','$contrasena','$email',now())");
-                if ($insertar) {
-                    echo "Te has registrado correctamente";
-                }
-            }else{
-                if($contarusuario > 0){
-                    echo "El usuario ya existe<br>.<a href='login.php'> Inicie Sesión </a><br>";
-                }
-                if($contaremail >0){
-                    echo "El email ya está en uso. <a href='login.php'> Inicie Sesión </a><br>";
-                }
-                if($contrasena != $repcontrasena) {
-                    echo "Las contraseñas no coinciden. Vuelva a intentarlo.<br>";
-                }
-            }
-        } 
-    ?>
 </body>
 </html>
