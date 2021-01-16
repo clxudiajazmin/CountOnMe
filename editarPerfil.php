0f8da4191c83c0d4974786392af6cabda53c35df
Claudia Jazmín Soria Saavedra <jazminsoria191998@gmail.com>
2021-01-16 16:57:37 +0100

    Actualizo perfil

--- editarPerfil.php
+++ editarPerfil.php
@@ -1,12 +1,17 @@
 <?php
     session_start();
+    ob_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
     <title>Editar Perfil</title>
+
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/norm.css">
 </head>
 <body>
     <?php
@@ -16,27 +21,137 @@
             $row = $solicitar->fetch_assoc();
         }
     ?>
-    <h1>Editar Perfil</h1> 
-    <form action="" method="post" enctype="multipart/form-data">
-        <input type="file" name="avatar" value ="<?php echo $row['avatar'];?>"><br>
-        <input type="text" placeholder="Nombre Completo" name="nombre" value ="<?php echo $row['nombre'];?>" required><br>
-        <input type="email" placeholder="Email" name="email" value ="<?php echo $row['email'];?>" required><br>
-        <input type="text" placeholder="Usuario" name="usuario" value ="<?php echo $row['usuario'];?>" required><br>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <!-- HEADER-->
+    <div id ="flipkart-navbar">
+      <div class="container">
+        <div class="row row1">
+          <div class="row row2">
+            <div class="col-sm-2">
+              <h1 style="margin:0px;"><span class="largenav">Count on Me</span></h1>
+            </div>
+            </div>
+        </div>
+      </div>
+    </div>
 
-        <input type="radio" name="sexo"
-        <?php if (isset($sexo) && $sexo=="Mujer" || $row['sexo'] =="Mujer") echo "checked";?>
-        value="Mujer">Mujer
-        <input type="radio" name="sexo"
-        <?php if (isset($sexo) && $sexo=="Hombre" || $row['sexo'] =="Hombre") echo "checked";?>
-        value="Hombre">Hombre
-        <input type="radio" name="sexo"
-        <?php if (isset($sexo) && $sexo=="Otro" || $row['sexo'] =="Otro") echo "checked";?>
-        value="Otro">Otro <br>
-        <input type="date" placeholder="Fecha de Nacimiento" name="nacimiento" value="<?php echo $row['nacimiento'];?>" required><br>
-        <textarea rows="10" cols="50" name ="descripcion" placeholder="Describe para que los demás conozcan un poco sobre ti."><?php echo $row['descripcion'];?></textarea><br>
-        <input type="submit" name="act" value="Actualizar Perfil"><br>
-    </form>
     <br>
+    <div class="container bootstrap snippet">
+      <form action="" method="post" enctype="multipart/form-data" id="registrationForm">  
+      <div class="row">
+        <div class="col-sm-3"><!--left col-->
+          <div class="text-center">
+            <img src = "<?php echo $row['avatar']; ?>" class="avatar img-circle img-thumbnail" alt="avatar">
+            <h6>Sube una nueva foto de perfil</h6>
+            <input type="file" name="avatar" value ="<?php echo $row['avatar'];?>" class="text-center center-block file-upload fotomaxwidth">
+          </div>
+        </div><!--/col-3-->
+        	<div class="col-sm-9">
+              <div class="tab-content">
+                <div class="tab-pane active" id="home">
+                    <hr>
+                          <div class="form-group">
+                              <div class="col-xs-6">
+                                  <label for="nombre"><h4>Nombre</h4></label>
+                                  <input type="text" class="form-control" id="nombre" placeholder="Nombre" title="Introduce tu nombre." name="nombre" value ="<?php echo $row['nombre'];?>">
+                              </div>
+                          </div>
+
+                          <div class="form-group">
+                              <div class="col-xs-6">
+                                <label for="username"><h4>Usuario</h4></label>
+                                  <input type="text" class="form-control" id="username" placeholder="Username" title="Introuduce tu nombre de usuario." name="usuario" value ="<?php echo $row['usuario'];?>">
+                              </div>
+                          </div>
+
+
+                          <div class="form-group">
+                              <div class="col-xs-6">
+                                  <label for="email"><h4>Email</h4></label>
+                                  <input type="email" class="form-control" id="email" placeholder="you@email.com" title="Introduce tu email." name="email" value ="<?php echo $row['email'];?>">
+                              </div>
+                          </div>
+
+                          <div class="form-group">
+                              <div class="col-xs-6">
+                                  <label for="fechanacimiento"><h4>Fecha de Nacimiento</h4></label>
+                                  <input type="date" class="form-control" id="fechanacimiento" placeholder="Fecha de Nacimiento" title="Introduce tu fecha de nacimiento" name="nacimiento" value="<?php echo $row['nacimiento'];?>">
+                              </div>
+                          </div>
+
+                          <div class="form-group">
+                            <div class="col-xs-6">
+                                <label for="text"><h4>Sexo</h4></label>
+                                    <select class="form-control" name = "sexo">
+                                        <option value="Masculino">Masculino</option>
+                                        <option value="Femenino">Femenino</option>
+                                        <option value="No binario">No binario</option>
+                                        <option value="Prefiero no identificar">Prefiero no identificar</option>
+                                    </select>
+                            </div>
+                        </div>
+
+                          <div class="form-group">
+
+                              <div class="col-xs-6">
+                                  <label for="descripcion"><h4>Descripcion</h4></label>
+                                  <textarea class="form-control" rows="5" cols="5" name ="descripcion" placeholder="Describe para que los demás conozcan un poco sobre ti."><?php echo $row['descripcion'];?></textarea><br>
+
+                          </div>
+
+                          <div class="form-group">
+
+                              <div class="col-xs-6">
+                                  <label for="password"><h4>Contraseña</h4></label>
+                                  <input type="password" class="form-control" name="contrasena" id="password" placeholder="Contraseña" title="Introduce tu contraseña.">
+                              </div>
+                          </div>
+                          <div class="form-group">
+
+                              <div class="col-xs-6">
+                                <label for="password2"><h4>Verifica tu contraseña</h4></label>
+                                  <input type="password" class="form-control" name="repcontrasena" id="password2" placeholder="Contraseña" title="Introduce tu contraseña.">
+                              </div>
+                          </div>
+                          <div class="form-group">
+                               <div class="col-xs-12">
+                                    <br>
+                                  	<button class="btn btn-lg btn-success" type="submit" name = "act"> Guardar</button>
+                                   	<button class="btn btn-lg" type="reset">Resetear</button>
+                                </div>
+                          </div>
+                  	</form>
+
+                  <hr>
+
+                 </div><!--/tab-pane-->
+                 <!--/tab-pane-->
+
+
+                  </div><!--/tab-pane-->
+              </div><!--/tab-content-->
+
+            </div><!--/col-9-->
+      </div><!--/row-->
+
+<br>
+<br>
+<br>
+    <!--FOOTER-->
+      <section id = "footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center">
+              <p><i>Count on Me</i> es un proyecto desarrollado por Sofía Martínez Parada y Claudia Jazmín Soria Saavedra para la asignatura de Desarrollo Web y de Apps</p>
+              <p class="h6">© All rights reserved.</p>
+            </div>
+            <hr>
+          </div>
+        </div>
+      </section>
+
+
+    
     <?php
         if(isset($_POST['act'])){
             $carpeta = "fotos_perfil/";
@@ -44,13 +159,12 @@
             $ruta = $carpeta."id_".$_SESSION['id']."_fotoperfil.jpg";
             $nombre = $_POST['nombre'];
             $email = $_POST['email'];
-            $usuario = $_POST['usuario'];
             $sexo = $_POST['sexo'];
+            $usuario = $_POST['usuario'];
             $nacimiento = $_POST['nacimiento'];
             $descripcion = $_POST['descripcion'];
 
             if(move_uploaded_file($_FILES['avatar']['tmp_name'], $carpeta."id_".$_SESSION['id']."_fotoperfil.jpg")){
-                echo "Fotos";
                 $insertar = $connect->query("UPDATE usuarios SET nombre ='$nombre', avatar = '$ruta', email = '$email', usuario = '$usuario', sexo = '$sexo', descripcion = '$descripcion', nacimiento = '$nacimiento' WHERE id = '".$_SESSION['id']."'");
                 echo "Datos actualizados correctamente.";
                 header("Refresh: 2; url = index.php");
--- perfil.php
+++ perfil.php
@@ -7,7 +7,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Evento</title>
+    <title>Perfil</title>
+
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/norm.css">
 </head>
 <body>
     <?php
@@ -16,6 +21,39 @@
             $solicitar = $connect->query("SELECT  * FROM usuarios WHERE id = '".$_SESSION['id']."'");
             $row = $solicitar->fetch_assoc();
     ?>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+    <!-- HEADER-->
+    <div id ="flipkart-navbar">
+    <div class="container">
+        <div class="row row1">
+        <ul class= "largenav pull-right">
+            <li class="upper-links"><a class="links" href="misAmigos.php">AMIGOS</a></li>
+            <li class="upper-links"><a class="links" href="misEventos.php">MIS EVENTOS</a></li>
+            <li class="upper-links"><a class="links" href="crearEvento.php">CREAR EVENTO</a></li>
+            <li class="upper-links"><a class="links" href="perfil.php"><?php echo strtoupper($row['nombre']) ?></a></li>
+            <li class="upper-links"><a class="links" href="logout.php">SALIR</a></li>
+
+        </ul>
+        <div class="row row2">
+            <div class="col-sm-2">
+            <h1 style="margin:0px;"><span class="largenav">Count on Me</span></h1>
+            </div>
+            <div class="flipkart-navbar-search smallsearch col-sm-8 col-xs-11">
+            <div class="row">
+                <input class="flipkart-navbar-input col-xs-11" type="text" id="buscador" placeholder="Busca tu evento" name = "textobusc">
+                <button class="flipkart-navbar-button col-xs-1" id = "boton" name="buscar">
+                    <svg width="15px" height="15px">
+                        <path d="M11.618 9.897l4.224 4.212c.092.09.1.23.02.312l-1.464 1.46c-.08.08-.222.072-.314-.02L9.868 11.66M6.486 10.9c-2.42 0-4.38-1.955-4.38-4.367 0-2.413 1.96-4.37 4.38-4.37s4.38 1.957 4.38 4.37c0 2.412-1.96 4.368-4.38 4.368m0-10.834C2.904.066 0 2.96 0 6.533 0 10.105 2.904 13 6.486 13s6.487-2.895 6.487-6.467c0-3.572-2.905-6.467-6.487-6.467 "></path>
+                    </svg>
+                </button>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+    
         <div>
             <h1> <?php echo $row['nombre']?></h1>
             <!--Esta es la barra superior, donde va el logo y el nombre y logout.-->
